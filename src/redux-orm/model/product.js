ca6c8ff58d3a3b46c065b529563791178e85fff0
Kostanios <kotov.const@mail.ru>
2021-08-30 11:40:15 +0300

    feat: get all reducer

--- src/redux-orm/model/product.js
+++ src/redux-orm/model/product.js
@@ -2,22 +2,21 @@ import { Model, attr } from 'redux-orm';
 
 class Product extends Model {
   static reducer = (action, Product) => {
-    console.log(Product)
     switch (action.type) {
         case 'CREATE_PRODUCT':
             Product.create(action.payload);
-            console.log(Product.first())
             break;
         case 'UPDATE_PRODUCT':
             Product.withId(action.payload.id).update(action.payload);
             break;
         case 'REMOVE_PRODUCT':
-            const product = Product.withId(action.payload);
-            product.delete();
+            Product.withId(action.payload).delete();
             break;
         case 'REMOVE_ALL_PRODUCT':
-            Product.all().delete()
-        default: break
+            Product.all().delete();
+            break;
+        case 'GET_ALL' :
+            return Product.all().toModelArray()
         }
   }
 }
