a1218cd63d7c9d7bd6a5d0bb5782564768329574
Kostanios <kotov.const@mail.ru>
2021-08-30 11:37:30 +0300

    fix: replace app file

---
+++ src/component/App/App.js
@@ -0,0 +1,29 @@
+import session from '../redux-orm/orm'
+import './App.css';
+import useModalReducer from "../hooks/useReducer";
+import Header from "./Header/header";
+
+function App() {
+  const createProduct = useModalReducer(
+      {type: 'CREATE_PRODUCT', payload:{id: 1, name: 'name'}},
+        session.Product
+  )
+  const getAllProducts = useModalReducer(
+      {type: 'GET_ALL'},
+      session.Product,
+      false
+  )
+  return (
+    <div className="App">
+      <Header/>
+      <button onClick={()=>{
+        createProduct()
+      }}>
+        create product
+      </button>
+      {getAllProducts().map(prod => <div>{`${prod}`}</div>)}
+    </div>
+  );
+}
+
+export default App;
